eab3477127ac883fea97f4a55232f0c3a28f590c
wang cen <wangcen361@163.com>
2019-06-14 19:39:28 +0800

    修改

--- scripts/test_lianxi.py
+++ scripts/test_lianxi.py
@@ -1,7 +1,7 @@
 class TestLogin:
     def test_01(self):
-        assert 0
+        assert 1
     def test_02(self):
-        assert 0
+        assert 1
     def test_03(self):
-        assert 0
+        assert 1
